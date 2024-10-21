d433033a67738ce2f83da5754268cff995b8a863
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2024-10-21 14:05:06 -0400

    Fix eslint errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-      proxy: {
-        // Note that your Express routes must start with /api 
-        // for the proxy to work
-        '/api': {
-          target: 'http://localhost:3000/',
-          changeOrigin: true
-        },
+    proxy: {
+      // Note that your Express routes must start with /api 
+      // for the proxy to work
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true
       },
     },
+  },
 });
