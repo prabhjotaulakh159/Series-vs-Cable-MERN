3002e73e43a5f71c1acc3912335ed3ce15a80d38
Bianca Rossetti <biancaros05@icloud.com>
2024-10-22 18:06:09 -0400

    Add method (and necessary helpers) to filter series

--- server/data-init.mjs
+++ server/data-init.mjs
@@ -1,4 +1,7 @@
 import API_KEY from './api.key.mjs';
+import {TOKEN} from './bin/www';
+
+const MIN_AIR_DATE = new Date('1990-01-01');
 
 async function fetchToken() {
   const response = await fetch('https://api4.thetvdb.com/v4/login', {
@@ -19,4 +22,76 @@ async function fetchToken() {
   return json.data.token;
 }
 
-export {fetchToken};
+const countries = ['can', 'usa', 'gbr'];
+
+async function fetchAllSeries() {
+  const response = await fetch('https://api4.thetvdb.com/v4/series', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${TOKEN}`
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`Not 2xx response, ${response.status}`, 
+      {cause: response});
+  }
+
+  const json = await response.json();
+  const series = json.data;
+
+  series.filter(show => { 
+    const airDate = new Date(show.firstAired);
+    return countries.includes(show.originalCountry) && 
+    airDate >= MIN_AIR_DATE;
+  });
+
+  const extendedSeriesResponses = await Promise.all(
+    series.map(show => fetch(`https://api4.thetvdb.com/v4/series/${show.id}/extended?short=true`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${TOKEN}`
+      }
+    }))
+  );
+
+  const extendedSeries = await Promise.all(extendedSeriesResponses.map(response => 
+    response.json()
+  ));
+
+  const filteredSeries = extendedSeries.map(series => series.data).filter(show => {
+    return isCableOrStreaming(show);
+  }).map(show => {
+    return {
+      'id': show.id,
+      'name': show.name,
+      'score': show.score,
+      'numberOfSeasons': getNumberOfSeasons(show),
+      'genres': getGenres(show),
+      'company': show.originalNetwork.parentCompany.name || show.originalNetwork.name,
+      'artwork': show.image,
+      'year': show.firstAired.split('-')[0]
+    };
+  });
+
+  return filteredSeries;
+}
+
+function getGenres(show) {
+  return show.genres.map(genre => genre.name);
+}
+
+function getNumberOfSeasons(show) {
+  const seasons = show.seasons.filter(season => season.type.name === 'Aired Order');
+
+  return seasons.length;
+}
+
+function isCableOrStreaming(show) {
+  const originalNetwork = show.originalNetwork ? show.originalNetwork.tagOptions : undefined;
+  return originalNetwork && 
+  (originalNetwork[0].name.toLowerCase().includes('cable') 
+  || originalNetwork[0].name.toLowerCase().includes('svod'));
+}
+
+export {fetchToken, fetchAllSeries};
