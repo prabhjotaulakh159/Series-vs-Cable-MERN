8ec5c2fb1d240c037237d8282a7a935681e9e678
AdamSW1 <adamwintercs@gmail.com>
2024-10-22 10:52:32 -0400

    add route for get series by id

---
+++ server/routers/series-id.router.mjs
@@ -0,0 +1,8 @@
+import express from 'express';
+import * as series from 'controllers/series.controller.mjs';
+
+const router = express.Router();
+
+router.get('/series/:id', series.getSeriesById);
+
+export default router;
