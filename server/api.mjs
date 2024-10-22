d03b526e884a66f3e3dbf9494707a32463471573
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2024-10-22 10:23:36 -0400

    Add error handling middleware

--- server/api.mjs
+++ server/api.mjs
@@ -4,8 +4,14 @@ const app = express();
 
 app.use(express.static('../client/dist'));
 
-app.use((req, res) => {
-  return res.status(400).json({ error: 'Not found' });
+app.use((req, res, next) => {
+  const error = new Error('Route not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res) => {
+  return res.status(error.status).json({ message: error.message });
 });
 
 export default app;
---
+++ server/test/series.test.mjs
@@ -0,0 +1,8 @@
+import * as chai from 'chai';
+import { describe } from 'mocha';
+
+const expect = chai.expect;
+
+describe('Test getting series with and without query parameters', () => {
+  it('', () => {});
+});
