706a869bb2b8e0b70ecac7df6e4b3db7e0230ff3
Bianca Rossetti <biancaros05@icloud.com>
2024-10-22 18:47:29 -0400

    Add documentation for functions in data-init

--- server/data-init.mjs
+++ server/data-init.mjs
@@ -2,7 +2,12 @@ import API_KEY from './api.key.mjs';
 import {TOKEN} from './bin/www';
 
 const MIN_AIR_DATE = new Date('1990-01-01');
+const countries = ['can', 'usa', 'gbr'];
 
+/**
+ * This function retrieves the token needed to authorize fetches to the TV DB
+ * @returns {string} - represents the token valid for the whole session
+ */
 async function fetchToken() {
   const response = await fetch('https://api4.thetvdb.com/v4/login', {
     headers: {
@@ -22,8 +27,17 @@ async function fetchToken() {
   return json.data.token;
 }
 
-const countries = ['can', 'usa', 'gbr'];
-
+/**
+ * This function fetches all series (and their extended endpoints)
+ * and filters this information to include the needed JSON for our app.
+ * 
+ * A valid series falls within the following:
+ * - its country is either Great Britain, Canada, or USA (see countries list for codes)
+ * - its company is either a cable or streaming service
+ * - its release date is from 1990-01-01 and onwards 
+ * 
+ * @returns {JSON} - represents the filtered series
+ */
 async function fetchAllSeries() {
   const response = await fetch('https://api4.thetvdb.com/v4/series', {
     headers: {
@@ -77,16 +91,34 @@ async function fetchAllSeries() {
   return filteredSeries;
 }
 
+/**
+ * This function retireves the list of a show's genres
+ * 
+ * @param {JSON} show - the show to check
+ * @returns {Array} -  represents all genres of a TV show
+ */
 function getGenres(show) {
   return show.genres.map(genre => genre.name);
 }
 
+/**
+ * This calculates a given show's total seasons.
+ * 
+ * @param {JSON} show - the show to check
+ * @returns {Number} - the number of seasons of a show
+ */
 function getNumberOfSeasons(show) {
   const seasons = show.seasons.filter(season => season.type.name === 'Aired Order');
 
   return seasons.length;
 }
 
+/**
+ * This function will check to see if the show is either on a cable or streaming service company
+ *  
+ * @param {JSON} show - the show to check
+ * @returns {Boolean} - represents whether the show is either a cable or streaming service show
+ */
 function isCableOrStreaming(show) {
   const originalNetwork = show.originalNetwork ? show.originalNetwork.tagOptions : undefined;
   return originalNetwork && 
