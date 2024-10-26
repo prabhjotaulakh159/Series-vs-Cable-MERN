bb4a36a2e441d5e7918af955ae93c1b507589573
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2024-10-26 19:48:01 -0400

    Document controller functions

--- server/controllers/series.controller.mjs
+++ server/controllers/series.controller.mjs
@@ -1,3 +1,6 @@
+/**
+ * Validates the year, name and type query parameters
+ */
 function validateSeriesQueryParameters(req, res, next) {
   try {
     if ('year' in req.query) {
@@ -19,24 +22,44 @@ function validateSeriesQueryParameters(req, res, next) {
   }
 }
 
+/**
+ * Checks if the year is between 1980 and 2024
+ * @param { String } year - Year to validate
+ * @throws { Error } - If the validation fails
+ */
 function validateYear(year) {
-  if (isNaN(year) || Number(year) < 1980 || Number(year) > 2024) {
+  const MIN_DATE = 1980;
+  const MAX_DATE = 2024;
+  if (isNaN(year) || Number(year) < MIN_DATE || Number(year) > MAX_DATE) {
     throw new Error('Year must be between 1980 and 2024');
   } 
 }
 
+/**
+ * Checks if the name is not empty or blank
+ * @param { String } name - Name to validate
+ * @throws { Error } - If the validation fails
+ */
 function validateName(name) {
   if (!name || name?.trim() === '') {
     throw new Error('Name cannot be empty');
   }
 }
 
+/**
+ * Checks if the type is not empty, blank or not cable or streaming
+ * @param { String } type - Type to validate
+ * @throws { Error } - If the validation fails
+ */
 function validateType(type) {
   if (!type || type?.trim() === '' || !['cable', 'streaming'].includes(type)) {
     throw new Error('Type must be either cable or streaming');
   }
 }
 
+/**
+ * Fetches series from the database depending on the query parameters
+ */
 function getSeriesWithQueryParameters(req, res) {
   res.status(200);
   res.send([]);
