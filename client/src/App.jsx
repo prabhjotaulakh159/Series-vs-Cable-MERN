0f629d7e51c342f219e4517bfdaed53ef75a9069
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2024-10-28 18:45:02 -0400

    Get series from the API in react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [series, setSeries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try { 
+        const response = await fetch('/api/series');
+        if (!response.ok) {
+          throw new Error('Response did not return 200');
+        }
+        const data = await response.json();
+        setSeries(prev => {
+          return [prev, ...data];
+        });
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    })();    
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>Hello World</div>
+    <div>Hello World: {series.length}</div>
   );
 }
 export default App;
