00bae7e93ac420fdd4b5709c92ce6b4e7a68682d
Bianca Rossetti <biancaros05@icloud.com>
2024-11-05 10:18:59 -0500

    Implement Swagger UI

--- server/api.mjs
+++ server/api.mjs
@@ -3,11 +3,30 @@ import express from 'express';
 import seriesRouter from './routers/series.router.mjs';
 import companiesRouter from './routers/companies.router.mjs';
 import compression from 'compression';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
-app.use(compression());
 
-// add app.use compression
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'LDD Got Moves Like Swagger',
+    version: '1.0.0',
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routers/*.mjs'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(compression());
 
 app.use(express.static('../client/dist'));
 
--- server/routers/series.router.mjs
+++ server/routers/series.router.mjs
@@ -10,9 +10,63 @@ import
 const seriesRouter = express.Router();
 // const seriesIdRouter = express.Router({mergeParams: true});
 
+
+/**
+ * @swagger
+ * /api/series/{id}:
+ *   get:
+ *     summary: Retrieve a series by ID
+ *     description: Get a specific series by its unique ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the series to retrieve
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Series object
+ *       400:
+ *         description: Invalid ID supplied
+ *       404:
+ *         description: Series not found
+ */
 seriesRouter.use('/:id', validateId);
 seriesRouter.get('/:id', getSeriesById);
 
+
+/**
+ * @swagger
+ * /api/series:
+ *   get:
+ *     summary: Retrieve a list of series
+ *     description: Get a list of series, optionally filtered by query parameters.
+ *     parameters:
+ *       - in: query
+ *         name: year
+ *         schema:
+ *           type: integer
+ *           minimum: 1980
+ *           maximum: 2024
+ *         description: Year of release
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: Name of the series
+ *       - in: query
+ *         name: type
+ *         schema:
+ *           type: string
+ *           enum: [cable, streaming]
+ *         description: Type of series platform
+ *     responses:
+ *       200:
+ *         description: A list of series
+ *       400:
+ *         description: Invalid query parameters
+ */
 seriesRouter.use('/', validateSeriesQueryParameters);
 seriesRouter.get('/', getSeriesWithQueryParameters);
 
