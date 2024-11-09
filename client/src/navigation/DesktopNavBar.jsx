b13951a44c00bc173e80f0c1436ef3d68d63da7e
AdamSW1 <adamwintercs@gmail.com>
2024-11-09 13:36:23 -0500

    add className to filter buttons

--- client/src/navigation/DesktopNavBar.jsx
+++ client/src/navigation/DesktopNavBar.jsx
@@ -3,9 +3,9 @@ export default function DesktopNavBar(){
   return(
     <>
       <section id="desktopNav" className="navBar">
-        <div>Filter 1</div>
-        <div>Filter 2</div>
-        <div>Filter 3</div>
+        <div className="filterBtn">Filter 1</div>
+        <div className="filterBtn">Filter 2</div>
+        <div className="filterBtn">Filter 3</div>
       </section>
     </>
   );
