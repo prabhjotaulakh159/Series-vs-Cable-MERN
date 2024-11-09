cb5ac277ac7e667a202f77c515bdc063ada84c5e
AdamSW1 <adamwintercs@gmail.com>
2024-11-09 13:36:35 -0500

    add burger menu to mobile nav

--- client/src/navigation/MobileNavBar.jsx
+++ client/src/navigation/MobileNavBar.jsx
@@ -1,10 +1,12 @@
-
+import { IoMdMenu } from 'react-icons/io';
 
 export default function MobileNavBar(){
   return(
     <>
       <section id="mobileNav" className="navBar">
-        
+        <div id="menuDiv">
+          <IoMdMenu className="menu" size="30px"/>
+        </div>
       </section>
     </>
   );
