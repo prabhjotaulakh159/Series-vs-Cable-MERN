c591c91599252d7919042104cf41c9d1d8ddf714
Bianca Rossetti <biancaros05@icloud.com>
2024-11-09 11:59:08 -0500

    Remove display for list of series

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,16 @@ import Graph from './graphs/Graph.jsx';
 import { useState, useEffect, useCallback } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import SeriesList from './SeriesList';
 
-
+/**
+ * This function is a dumby function for now. in the future,
+ * each graph will have its own way of customly calculating the series.
+ * The catch: The functions for calculating the x and y axis MUST ALWAYS
+ * RETURN AN OBJECT {xAxis, yAxis}!!! This naming convention IS IMPORTANT!!!
+ * 
+ * @param {Array} series - list of all series 
+ * @returns - an object representing the x axis and y axis plots
+ */
 function calculateAllAxies(series) {
   const onlyYears = series.map(show => show.year).filter(year => year);
   const uniqueYears = Array.from(new Set(onlyYears));
@@ -22,8 +29,6 @@ function App() {
   const [series, setSeries] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-  const [selected, setSelected] = useState(undefined);
-  const [loadingSelected, setLoadingSelected] = useState(false);
 
   const calculateAxies = useCallback((data) => {
     return calculateAllAxies(data);
@@ -47,23 +52,6 @@ function App() {
     })();
   }, []); 
 
-
-  async function fetchIndividualSeries(id) {
-    try {
-      setLoadingSelected(true);
-      const response = await fetch(`/api/series/${id}`);
-      if (!response.ok) {
-        throw new Error('Response did not return 200');
-      }
-      const data = await response.json();
-      setSelected(data);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoadingSelected(false);
-    }
-  }
-
   if (loading) {
     return <Skeleton width={'25%'} count={50}/>;
   }
@@ -74,11 +62,7 @@ function App() {
 
   return (
     <div>
-      <h1>Currently selected series: 
-        {loadingSelected ? <Skeleton width={'25%'}/> : selected?.id}
-      </h1>
       <h1>All series: </h1>
-      <SeriesList series={series} fetchIndividualSeries={fetchIndividualSeries}/>
       <Graph calculateAxies={() => calculateAxies(series)}/>
     </div>
   );
