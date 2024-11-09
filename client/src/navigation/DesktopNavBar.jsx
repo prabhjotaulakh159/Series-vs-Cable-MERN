ce9d71c2e58678dd329dec417201e41b6d61421f
AdamSW1 <adamwintercs@gmail.com>
2024-11-09 12:59:25 -0500

    add simple desktop nav component

---
+++ client/src/navigation/DesktopNavBar.jsx
@@ -0,0 +1,12 @@
+
+export default function DesktopNavBar(){
+  return(
+    <>
+      <section id="desktopNav" className="navBar">
+        <div>Filter 1</div>
+        <div>Filter 2</div>
+        <div>Filter 3</div>
+      </section>
+    </>
+  );
+}
