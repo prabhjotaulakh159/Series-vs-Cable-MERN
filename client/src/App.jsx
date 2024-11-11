ad2976e4162f12a7a07d2a405f063960992825bf
AdamSW1 <adamwintercs@gmail.com>
2024-11-10 21:18:43 -0500

    add function to get yearly score average by company type

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,58 @@ function calculateAllAxies(series) {
   return {xAxis, yAxis};
 }
 
+/**
+ * This function is used in the line chart for the average scores per year
+ * of cable versus streaming services.
+ * Because each line chart needs an x axis and a y axis, this function returns
+ * an array with 2 {xAxis, yAxis} objects 
+ * 
+ * @param {Array} series - list of all series 
+ * @returns  - an array with 2 objects representing the x axis and y axis plots
+ */
+function calculateCompanyScoresPerYear(series){
+
+  const onlyYears = series.map(show => show.year).filter(year => year);
+  const uniqueYears = Array.from(new Set(onlyYears));
+  let yAxis = uniqueYears.sort((a, b) => a - b);
+  yAxis = yAxis.filter( year => year < 2025);
+
+  const cableSeries = series.filter( show => 
+    show.companyType === 'cable' && Number(show.year) < 2025);
+
+  const streamingSeries = series.filter( show =>
+    show.companyType === 'streaming' && Number(show.year) < 2025);
+
+  const cableSeriesByYear = Object.groupBy(cableSeries, ({year}) => year  );
+  const streamingSeriesByYear = Object.groupBy(streamingSeries, ({year}) => year);
+
+
+  const cableAverageScores = [];
+
+  Object.keys(cableSeriesByYear).forEach(year => {
+    let totalScoreForYear = 0;
+    cableSeriesByYear[year].forEach(show => {
+      totalScoreForYear += show.score;
+    });
+    const averageScore = totalScoreForYear / year.length;
+    cableAverageScores.push(averageScore);
+  });
+
+  const streamingAverageScores = [];
+
+  Object.keys(streamingSeriesByYear).forEach(year => {
+    let totalScoreForYear = 0;
+    streamingSeriesByYear[year].forEach(show => {
+      totalScoreForYear += show.score;
+    });
+    const averageScore = totalScoreForYear / year.length;
+    streamingAverageScores.push(averageScore);
+  });
+
+  return [{cableAverageScores, yAxis}, {streamingAverageScores, yAxis}];
+  
+}
+
 function App() {
   const [series, setSeries] = useState([]);
   const [loading, setLoading] = useState(false);
