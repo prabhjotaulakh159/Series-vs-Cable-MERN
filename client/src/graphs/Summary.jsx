482dfed0390884f17473caccc83ac943b0b19dd1
Bianca Rossetti <biancaros05@icloud.com>
2024-11-12 11:04:35 -0500

    Add summary component to display a graph's summary

---
+++ client/src/graphs/Summary.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+import 'react-loading-skeleton/dist/skeleton.css';
+import './Summary.css';
+
+function Summary({fetchSummaryData, summaryTitle}) {
+  const [series, setSeries] = useState({});
+  useEffect(() => {
+    async function fetchData() {
+      const results = await fetchSummaryData();
+      setSeries(results);
+    }
+
+    fetchData();
+  }, [fetchSummaryData]);
+  return (
+    <section className="summary-block">
+      <h2>{summaryTitle}</h2>
+      {
+        Object.keys(series).map(type => {
+          return (
+            <div key={type} className="company-summary">
+              <h3>Winning show in {type}</h3>
+              <h3>{series[type].name}</h3>
+              <p>Score: {series[type].score}</p>
+              <p>Seasons: {series[type].numberOfSeasons}</p>
+              <p>Year of release: {series[type].year}</p>
+            </div>
+          );
+        })
+      }
+    </section>
+  );
+}
+
+export default Summary;
