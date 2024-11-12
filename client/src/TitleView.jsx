e0bfd1903c29b2eae3b26bd15acd2927fa2006cd
Bianca Rossetti <biancaros05@icloud.com>
2024-11-12 17:36:38 -0500

    Fix padding and font of app

--- client/src/TitleView.jsx
+++ client/src/TitleView.jsx
@@ -13,13 +13,16 @@ function TitleView() {
         in streaming services, who offered the ability to watch shows as well. The major 
         difference: The shows were on demand. This meant the ability to watch and rewatch 
         your favourite shows, and without the constant ads (though recently, this comes at 
-        a greater price). <br/>When streaming services were limited, this was great. But as 
+        a greater price). 
+      </p>
+      <p>When streaming services were limited, this was great. But as 
         companies decided to buy out their rights and split off into their own companies, 
         many users have wondered if streaming services are still as good as they were. 
         The frequency at which streaming services cancel shows last minute has also affected 
         viewers&apos; opinions. So, we attempted to gather and display data to give users a 
         real visual of what exactly is going on here, and perhaps attempt at answering 
-        the question: Cable or streaming? </p>
+        the question: Cable or streaming? 
+      </p>
     </section>
   );
 }
