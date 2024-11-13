60932684ef0151a4d2c4407767c821748a4025f4
AdamSW1 <adamwintercs@gmail.com>
2024-11-12 21:14:59 -0500

    add useEffect to navBar for navigation

--- client/src/navigation/NavBar.jsx
+++ client/src/navigation/NavBar.jsx
@@ -2,8 +2,24 @@ import './nav.css';
 import Logo from '../assets/favicon-tv.png';
 import DesktopNavBar from './DesktopNavBar';
 import MobileNavBar from './MobileNavBar';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function NavBar() {
+
+  // Use React Router's useLocation hook to detect changes in the URL
+  const location = useLocation();
+
+  useEffect(() => {
+    // Scroll to the element when the component mounts or the hash changes
+    if (location.hash) {
+      const targetElement = document.getElementById(location.hash.replace('#', ''));
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
+
   return (
     <nav>
       <div className="Logo">
